719177a2bdac3671969282630a68308c471df08f
Tanishi Devlal <tanishidevlal@gmail.com>
2023-02-05 01:59:05 +0530

    yo

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+import { AppBar, ToolBar } from '@mui/material';
+
+const Header = () => {
+
+    return (
+        <div>hello from header</div>
+    )
+}
+
+export default Header;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,9 @@
 
+import Header from "./Header";
+
 const Home = () => {
     return(
-        <div>hello</div>
+        <Header/>
     )
 }
 
